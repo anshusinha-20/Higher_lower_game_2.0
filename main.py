4d4e60a63e17fe1647e471870ff0766ead1b369c
Anshu Sinha <anshujuly2@gmail.com>
2023-04-25 13:29:48 +0530

    modify

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-# imported Flask class from flask module
-from flask import Flask
+# # imported Flask class from flask module
+# from flask import Flask
 
-# create an instance of the Flask class
-app = Flask(__name__)
+# # create an instance of the Flask class
+# app = Flask(__name__)
 
-def make_bold(function):
-    def wrapper():
-        return f"<b>{function()}</b>"
-    return wrapper
+# def make_bold(function):
+#     def wrapper():
+#         return f"<b>{function()}</b>"
+#     return wrapper
 
-def make_emphasis(function):
-    def wrapper():
-        return f"<em>{function()}</em>"
-    return wrapper
+# def make_emphasis(function):
+#     def wrapper():
+#         return f"<em>{function()}</em>"
+#     return wrapper
 
-def make_underlined(function):
-    def wrapper():
-        return f"<u>{function()}</u>"
-    return wrapper
+# def make_underlined(function):
+#     def wrapper():
+#         return f"<u>{function()}</u>"
+#     return wrapper
 
-# use the route() decorator to tell Flask what URL should trigger our function
-@app.route("/")
-@make_bold
-@make_emphasis
-@make_underlined
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+# # use the route() decorator to tell Flask what URL should trigger our function
+# @app.route("/")
+# @make_bold
+# @make_emphasis
+# @make_underlined
+# def hello_world():
+#     return "<h1>Hello, World!</h1>"
 
-# use the route() decorator to tell Flask what URL should trigger our function
-@app.route("/<name>")
-def greet(name):
-    return f"<h1>Hello {name}!</h1>"
+# # use the route() decorator to tell Flask what URL should trigger our function
+# @app.route("/<name>")
+# def greet(name):
+#     return f"<h1>Hello {name}!</h1>"
 
-# check if the executed file is the main program and run the app
-if __name__ == "__main__":
-    app.run(debug=True)
+# # check if the executed file is the main program and run the app
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
